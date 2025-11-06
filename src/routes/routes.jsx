7ce1dae81325baf19a48857a66f9e71d6f562901
SK Sadikul Islam <sksadikulislam431@gmail.com>
2025-11-06 17:59:57 +0530

    changes in route

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Route, Routes } from "react-router";
 import MainLayout from "../components/layouts/main-layout";
 import LandingPage from "../pages/landing-page";
-import CryptoLanding from "../components/Demo";
 
 const RoutesPage = () => {
   return (
@@ -11,7 +10,6 @@ const RoutesPage = () => {
         <Route path="/" element={<MainLayout />}>
           <Route index element={<LandingPage />} />
         </Route>
-        <Route path="/Demo" element={<CryptoLanding />} />
       </Routes>
     </>
   );
